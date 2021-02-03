572d141b166feca4f9194b04bdb837685090e904
Blizlike <Blizlike0gmail.com>
2021-02-03 22:17:17 +0300

    Positive

---
+++ src/Kata8/Positive.java
@@ -0,0 +1,24 @@
+package Kata8;
+
+public class Positive {
+	
+	public static int sum(int[] arr) {
+		
+		int result = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > 0) {
+				result += arr[i];
+			}
+		}
+		return result;
+	}
+}
+/* Sum of positive
+ *
+ * You get an array of numbers, return the sum of all of the positives ones.
+ *
+ * Example [1,-4,7,12] => 1 + 7 + 12 = 20
+ *
+ * Note: if there is nothing to sum, the sum is default to 0.
+ */
