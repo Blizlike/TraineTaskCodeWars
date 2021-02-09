d6d939205580e044a14c52bc8dec48a686b35239
Blizlike <Blizlike0@gmail.com>
2021-02-09 19:17:22 +0300

    ZywOo
    Signed-off-by: Blizlike <Blizlike0@gmail.com>

---
+++ src/Kata8/ZywOo.java
@@ -0,0 +1,29 @@
+package Kata8;
+
+public class ZywOo {
+	
+	public static String warnTheSheep(String[] array) {
+		if (array[array.length - 1].equals("wolf")) {
+			return "Pls go away and stop eating my sheep";
+		} else {
+			for (int i = array.length - 1; i >= 0; i--) {
+				if (array[i].equals("wolf")) {
+					return "Oi! Sheep number " + (array.length - (1 + i)) + "! You are about to be eaten by a wolf!";
+				}
+			}
+			return null;
+		}
+	}
+}
+/* A wolf in sheep's clothing
+ *
+ * Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+ *
+ * Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+ *
+ * [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+ *    7      6      5      4      3            2      1
+ * If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+ *
+ * Note: there will always be exactly one wolf in the array.
+ */
