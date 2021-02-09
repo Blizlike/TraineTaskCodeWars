88b03a9e168a70d0c68b5bcf00bbe26777fc1da9
Blizlike <Blizlike0@gmail.com>
2021-02-09 16:18:20 +0300

    School
    Signed-off-by: Blizlike <Blizlike0@gmail.com>

---
+++ src/Kata8/School.java
@@ -0,0 +1,21 @@
+package Kata8;
+
+public class School {
+	
+	public static int getAverage(int[] marks) {
+		int sum = 0;
+		for (int mark : marks) {
+			sum += mark;
+		}
+		return sum / marks.length;
+	}
+}
+/* Get the mean of an array
+ *
+ * It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students
+ * come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+ *
+ * Return the average of the given array rounded down to its nearest integer.
+ *
+ * The array will never be empty.
+ */
